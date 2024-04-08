4362b4b18cc0ef4d0a117c2c1036a8240d2b5310
Faizan Ahmad <140067601+Faizan-glitch-os@users.noreply.github.com>
2024-04-08 15:34:20 +0500

    Add files via upload

---
+++ InHome_task13_multiplicationOfEnteredNumber.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  print('Enter a Number to show its table: ');
+  int userNumber = int.parse(stdin.readLineSync()!);
+
+  for (int i = 1; i <= 10; i++) {
+    int table = userNumber * i;
+    print('$userNumber x $i = $table');
+  }
+}
